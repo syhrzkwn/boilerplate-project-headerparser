b55f612f958d990a42b60cd33e14cecddd903287
syhrzkwn <syahirzakwan.dev@gmail.com>
2024-05-14 20:07:59 +0800

    Create whoami API

--- index.js
+++ index.js
@@ -24,6 +24,18 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  const ipadress = req.ip || req.headers['x-forwarded-for']
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  res.json({
+    ipadress: ipadress,
+    language: language,
+    software: software
+  });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
